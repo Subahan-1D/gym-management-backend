84dc076a358a27179f77111a5e69f34f1711cbd0
Md. Subahan Ali <subahanislam523@gmail.com>
2025-08-17 23:25:08 +0600

    added to typscript eslint erro handle

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import cors from "cors";
 import { notFound } from "./app/middlewares/notFound";
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 
